4e035a9d589f7c3319c75a77c8ff37c16dd39fc4
Aayush Gupta <aayushgupta69official@gmail.com>
2025-04-16 18:47:12 +0530

    Added constants and navbar.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Navbar from "./components/navbar.jsx";
+
 const App = () => {
   return (
-    <>App</>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,38 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 15, suffix: "+", label: "Years of Experience" },
+  { value: 200, suffix: "+", label: "Satisfied Clients" },
+  { value: 108, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
+
+export { words, navLinks, counterItems };
