76e99ddbca10c26f33672a34477baeebfc877dcf
Aayush Gupta <aayushguptaworks@gmail.com>
2026-02-11 19:10:57 +0530

    Added words

--- constants/index.tsx
+++ constants/index.tsx
@@ -16,3 +16,14 @@ export const navLinks = [
     link: "#testimonials",
   },
 ];
+
+export const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
